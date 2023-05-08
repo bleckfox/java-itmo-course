879ff15dbc0ae205e42f99266d893f3fedb4bbec
master <79881790355@ya.ru>
2023-05-08 15:32:14 +0300

    feat: Add solve for task 3

---
+++ src/homeWork3/Helper.java
@@ -0,0 +1,38 @@
+package homeWork3;
+
+import java.util.Scanner;
+
+public class Helper {
+    /**
+     * Метод проверки типа числа
+     * @param value значение
+     * @return является ли число целым типом
+     */
+    private static boolean isInt(String value){
+        try {
+            Integer.parseInt(value);
+            return true;
+        }
+        catch (NumberFormatException e){
+            return false;
+        }
+    }
+
+    /**
+     * Метод получения положительного целого числа, введенного пользователем
+     * @param scanner сканнер ввода
+     * @return введенное число
+     */
+    public static int getPositiveUserInput(Scanner scanner){
+        if (scanner != null){
+            var input = scanner.next();
+
+            if(isInt(input)){
+                return Integer.parseInt(input);
+            }
+
+            return -1;
+        }
+        return -1;
+    }
+}
---
+++ src/homeWork3/Task1.java
@@ -0,0 +1,18 @@
+package homeWork3;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        int cycleCounter = 0;
+
+        for (int i = 2; i < 100; i+=2) {
+            if(cycleCounter != 10){
+                cycleCounter++;
+                System.out.printf("%d -> %d\n", cycleCounter, i);
+                continue;
+            }
+            break;
+        }
+
+    }
+}
